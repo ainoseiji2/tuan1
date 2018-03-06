8383c0766c3fb3514589d36b02df7702d8fb2586
khang <sagasggas@gmail.com>
2018-03-06 16:52:41 +0700

    ipcucbo

---
+++ lan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+using System.Net;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String strHostName = Dns.GetHostName();
+            Console.WriteLine("Host Name: " + strHostName);
+            IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+            int nIP = 0;
+            foreach (IPAddress ipaddress in iphostentry.AddressList)
+            {
+                Console.WriteLine("IP #" + ++nIP + ": " +
+                                  ipaddress.ToString());
+            }
+        }
+    }
+}
