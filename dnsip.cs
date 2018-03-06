fa4ab3b2c2a9d70da0eacff762844b9d2b6a4529
khang <sagasggas@gmail.com>
2018-03-06 17:38:05 +0700

    thembai

---
+++ dnsip.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string DnSAdd = "www.tinhte.vn";
+            IPHostEntry entry = Dns.Resolve(DnSAdd);
+            Console.WriteLine(entry.AddressList[0]);
+            Console.WriteLine(entry.HostName);
+        }
+    }
+}
